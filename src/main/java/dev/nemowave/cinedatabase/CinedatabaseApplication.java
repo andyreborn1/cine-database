8d83301ad0ad58e8b09213013973cffff4ba0153
Vinicius Ribeiro <andyreborn1@gmail.com>
2021-08-25 12:01:16 -0300

    Commit inicial

---
+++ src/main/java/dev/nemowave/cinedatabase/CinedatabaseApplication.java
@@ -0,0 +1,13 @@
+package dev.nemowave.cinedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinedatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinedatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nemowave/cinedatabase/CinedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nemowave.cinedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinedatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
