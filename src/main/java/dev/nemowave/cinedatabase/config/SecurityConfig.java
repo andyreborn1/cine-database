8c1cd26adedb2c2bb345c42d5fa61f8e0d8fb31b
Anderson VInicius Ribeiro <andyreborn@hotmail.com.br>
2021-09-01 09:43:01 -0300

    Modificação no security

--- src/main/java/dev/nemowave/cinedatabase/config/SecurityConfig.java
+++ src/main/java/dev/nemowave/cinedatabase/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package dev.nemowave.cinedatabase.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +15,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity security) throws Exception {
         security.httpBasic().disable();
         security.cors().and().csrf().disable();
+
+//        security.authorizeRequests().anyRequest()
+//                .authenticated().and()
+//                .formLogin();
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
 
